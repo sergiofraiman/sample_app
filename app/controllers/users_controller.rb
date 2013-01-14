f04a24b728ee81c84d4d2ad9aaf1dded1f61c6e5
hungreebr <marketing@hungree.com.br>
2013-01-14 16:26:12 -0300

    alteracoes geral

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,13 +9,13 @@ class UsersController < ApplicationController
   end
   
   def create
-  @user = User.new(params[:user])
-  if @user.save
-	sign_in @user
-	flash[:success] = "Bem vindo ao Twitter Teste!"
-	redirect_to @user
-  else 
-    render 'new' 
+    @user = User.new(params[:user])
+    if @user.save
+      sign_in @user
+      flash[:success] = "Bem vindo ao Twitter Teste!"
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
-end
 end
