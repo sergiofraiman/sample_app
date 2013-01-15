ef75eeb0d5bd79a8b41d29532bea73395f6a8b17
hungreebr <marketing@hungree.com.br>
2013-01-15 01:47:23 -0300

    alterando users helper

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 ﻿class UsersController < ApplicationController
-before_filter :signed_in_user, only: [:index, :edit, :update]
-before_filter :correct_user,   only: [:edit, :update]
+  before_filter :signed_in_user, only: [:index, :edit, :update]
+  before_filter :correct_user,   only: [:edit, :update]
 
   def index
   @users = User.all
