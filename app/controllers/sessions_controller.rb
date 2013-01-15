29db6e0daf7c42360795ec3bc77911cea4dd4b27
hungreebr <marketing@hungree.com.br>
2013-01-14 22:41:02 -0300

    Implementando Login-out, parte 1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,8 @@ end
 def create
 	user = User.find_by_email(params[:session][:email].downcase)
 	if user && user.authenticate(params[:session][:password])
+	 sign_in user
+      redirect_back_or user
  else
   flash.now[:error] = 'E-mail/Senha incorretos. Tente novamente'
   render 'new'
